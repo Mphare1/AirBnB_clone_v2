7ada71302ef8d3b2bea82e9139045b89801e4a2b
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-10-24 21:33:14 +0200

    Update 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
-from flask import Flask
 """
 intialize flask web app to listen on 0.0.0.0:5000
 """
 
+from flask import Flask
+
+
 app = Flask(__name__)
-app.url_map.strict_slashes = False
-
-@app.route('/')
-def hello_world():
-    """
-    Display "Hello HBNB!"
-    """
-    return ('Hello HBNB!')
 
 
-@app.route('/hbnb')
-def hbnb():
-    """
-    route /hbnb displays
-    """
-    return ('HBNB')
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_hello():
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
