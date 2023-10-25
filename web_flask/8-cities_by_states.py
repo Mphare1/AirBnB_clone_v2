98eba9822d81d3c432e542f4c3a66f414873ab80
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-10-25 11:00:59 +0200

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -2,31 +2,40 @@
 """
 intialize flask web app to listen on 0.0.0.0:5000
 """
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+from operator import attrgetter
 
-from flask import Flask, render_template
-from models import storage, classes
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
-@app.route('/states_list')
+@app.route('/states_list', strict_slashes=False)
 def states_list():
-    """
-    """
-    states = storage.all(classes["State"]).values()
-    return (render_template('8-cities_by_states.html', states=states))
+    """retrieve states from database and render templates based on results"""
+    states = []
+    all_states = storage.all(State)
+    for val in all_states.values():
+        states.append(val)
+    states = sorted(states, key=attrgetter("name"))
+    return render_template('7-states_list.html', state=state)
 
-@app.route('/cities_by_states')
+@app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """
-    """
-    states = storage.all(classes["State"]).values()
-    return (render_template('8-cities_by_states.html', states=states))
+    """retrieve cities by states fro database and render template"""
+    states = []
+    all_states = storage.all(State)
+    for state in all_states.values():
+        states.append(state)
+
+    states = sorted(states, key=attrgetter("name"))
+    return render_template('8-cities_by_states.html', states=states)
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """
-    """
+def teardown(self):
+    """closes the current session after each request"""
     storage.close()
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
