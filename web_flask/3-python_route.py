f019a060a7fd569be4e499c73fb0fc6ad730939a
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-10-25 10:54:18 +0200

    Update 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,40 +1,35 @@
 #!/usr/bin/python3
-from flask import Flask
 
 """
 intializing flask web app to listen on 0.0.0.0:5000
 """
+from flask import Flask
+
+
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
-@app.route('/')
-def hello_world():
-    """
-    Display "Hello HBNB!"
-    """
-    return ('Hello HBNB!')
 
-@app.route('/hbnb')
-def hbnb():
-    """
-    route /hbnb displays
-    """
-    return ('HBNB')
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
 
-@app.route('/c/<text>')
-def c(text):
-    """
-    replaces _ with spaces
-    """
-    return ("C {}".format(text.replace('_', ' ')))
 
-@app.route('/python')
-@app.route('/python/<text>')
-def py(text="is cool"):
-    """
-    default adds is cool and replaces _ with space
-    """
-    return("Python {}".format(text.replace('_', ' ')))
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_hello():
+    return "HBNB"
 
-if __name__ == "__main__":
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    return "C is {}".format(text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text='is cool'):
+    return "Python {}".format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
     app.run(host="0.0.0.0", port=5000)
