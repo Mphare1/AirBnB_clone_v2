e7d3505d3ec1f528505f215be63feb0eaee2f0d6
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-10-25 20:12:46 +0200

    Create amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+State Module
+"""
+
+from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+
+class Amenity(BaseModel, Base):
+    """ Amenities """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   viewonly=False)
