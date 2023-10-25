045d7f23fcd92048807715d4f3f8f14ef1ec1b5a
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-10-25 16:52:05 +0200

    Update state.py

--- models/state.py
+++ models/state.py
@@ -3,34 +3,34 @@
 '''
     Implementation of the State class
 '''
-
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from models import storage
 from models.city import City
-import os
-import models
+from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from os import getenv
+
 
 class State(BaseModel, Base):
-    '''
-        Implementation for the State.
-    '''
-    __tablename__ = 'states'
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", passive_deletes=True, backref="state")
-    else:
-        name = ""
+    """ State table
+    Attributes:
+        __tablename__: the database table
+        name: state name
+        cities: Defines a relationship to the cities table
+     """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="states", cascade="delete")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
-            """
-            cities property
+            """Get the cities whose states ids correspond to the current state
             """
             city_list = []
-            for key, val in models.storage.all().items():
-                try:
-                    if val.state_id == self.id:
-                        city_list.append(val)
-                except AttributeError:
-                    pass
+
+            for city in list(storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
             return city_list
